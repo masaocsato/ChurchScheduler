447e121aabde8bbe3504c1004716c5dea233987a
Masa Sato <masaocsato@yahoo.com>
2019-02-22 00:13:39 -0800

    added some menus/submenus and shortcuts

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for the app to be ready
 app.on("ready", function() {
@@ -19,4 +20,86 @@ app.on("ready", function() {
       slashes: true
     })
   );
+  // Quit app when closed
+  mainWindow.on("closed", function() {
+    app.quit();
+  });
+
+  // Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  // Insert menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle add role window
+function addRoleWindow() {
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add New Role"
+  });
+
+  // Load the HTML into window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  // Garbage collection handle
+  addWindow.on("close", function() {
+    addWindow = null;
+  });
+}
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Role",
+        click() {
+          addRoleWindow();
+        }
+      },
+      {
+        label: "test2"
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q", //  quit shortcut
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+// If mac, add empty object to menu
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
+}
+
+// Add dev tools if not in prod
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "DevTools",
+    submenu: [
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I", //  dev tools shortcut
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: "reload"
+      }
+    ]
+  });
+}
